df232509b6e2f78dc96aa6db83f9901ea28a4bb4
Shujia Huang <huangshujia9@gmail.com>
2022-01-10 21:42:30 +0800

    add namespace

--- setup.py
+++ setup.py
@@ -4,23 +4,26 @@ Version 1.0.0 (May 23, 2020)
 Copyright (c) 2020 Shujia Huang
 """
 import os
+from argparse import Namespace
+
+DESCRIPTION = ("qmplot: Create high-quality manhattan and QQ plots for PLINK association "
+               "output (or any dataframe with chromosome, position, and p-value).")
+meta = Namespace(
+    __DISTNAME__="qmplot",
+    __AUTHOR__="Shujia Huang",
+    __AUTHOR_EMAIL__="huangshujia9@gmail.com",
+    __URL__="https://github.com/ShujiaHuang/qmplot",
+    __LICENSE__="BSD (3-clause)",
+    __DOWNLOAD_URL__="https://github.com/ShujiaHuang/qmplot",
+    __VERSION__="0.1.8",
+)
 
 try:
     from setuptools import setup, find_packages
+
     _has_setuptools = True
 except ImportError:
     from distutils.core import setup, find_packages
-
-
-DESCRIPTION = ("qmplot: Create high-quality manhattan and QQ plots for PLINK association output (or "
-               "any dataframe with chromosome, position, and p-value).")
-DISTNAME = "qmplot"
-MAINTAINER = "Shujia Huang"
-MAINTAINER_EMAIL = "huangshujia9@gmail.com"
-URL = "https://github.com/ShujiaHuang/qmplot"
-LICENSE = "BSD (3-clause)"
-DOWNLOAD_URL = "https://github.com/ShujiaHuang/qmplot"
-VERSION = "0.1.7"
 
 INSTALL_REQUIRES = [
     "numpy",
@@ -32,19 +35,19 @@ INSTALL_REQUIRES = [
 ROOT_DIR = os.path.split(os.path.realpath(__file__))[0]
 
 if __name__ == "__main__":
-    setup(name=DISTNAME,
-          author=MAINTAINER,
-          author_email=MAINTAINER_EMAIL,
-          maintainer=MAINTAINER,
-          maintainer_email=MAINTAINER_EMAIL,
+    setup(name=meta.__DISTNAME__,
+          author=meta.__AUTHOR__,
+          author_email=meta.__AUTHOR_EMAIL__,
+          maintainer=meta.__AUTHOR__,
+          maintainer_email=meta.__AUTHOR_EMAIL__,
           description=DESCRIPTION,
           long_description=(open(ROOT_DIR + "/README.rst").read()),
-          license=LICENSE,
-          url=URL,
-          download_url=DOWNLOAD_URL,
+          license=meta.__LICENSE__,
+          url=meta.__URL__,
+          download_url=meta.__DOWNLOAD_URL__,
           packages=find_packages(),
           install_requires=INSTALL_REQUIRES,
-          version=VERSION,
+          version=meta.__VERSION__,
           include_package_data=True,
           entry_points={
               "console_scripts": [
@@ -52,11 +55,13 @@ if __name__ == "__main__":
               ]
           },
           classifiers=[
-             'Intended Audience :: Science/Research',
-             'Programming Language :: Python :: 3.7',
-             'License :: OSI Approved :: BSD License',
-             'Topic :: Scientific/Engineering :: Bio-Informatics',
-             'Operating System :: POSIX',
-             'Operating System :: POSIX :: Linux',
-             'Operating System :: MacOS'],
-    )
+              'Intended Audience :: Science/Research',
+              'Programming Language :: Python :: 3.7',
+              'Programming Language :: Python :: 3.8',
+              'Programming Language :: Python :: 3.9',
+              'License :: OSI Approved :: BSD License',
+              'Topic :: Scientific/Engineering :: Bio-Informatics',
+              'Operating System :: POSIX',
+              'Operating System :: POSIX :: Linux',
+              'Operating System :: MacOS'],
+          )
