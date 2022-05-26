e78410da3fadee86eeed031c7b86a54063fb3c08
Shujia Huang <huangshujia9@gmail.com>
2022-05-26 19:38:01 +0800

    set some default matplotlib.rcParams for plot

--- qmplot/__init__.py
+++ qmplot/__init__.py
@@ -1,3 +1,9 @@
+import matplotlib
 from .modules import manhattanplot, qqplot, qqnorm
+
+matplotlib.rcParams['ps.fonttype']     = 42
+matplotlib.rcParams['pdf.fonttype']    = 42
+matplotlib.rcParams['font.sans-serif'] = ["Arial","Lucida Sans","DejaVu Sans","Lucida Grande","Verdana"]
+matplotlib.rcParams['font.family']     = 'sans-serif'
 
 __all__ = ["manhattanplot", "qqplot", "qqnorm"]
